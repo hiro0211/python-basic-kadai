3c1f40b86877a1775d1db1af7a0742cf3d2fd359
hiro0211 <143386971+hiro0211@users.noreply.github.com>
2023-09-20 15:06:37 +0900

    Update python_課題14.py

--- python_課題14.py
+++ python_課題14.py
@@ -9,9 +9,10 @@ Original file is located at
 
 price1 = 100
 price2 = 200
+tax = 1.1
 
 def total():
-    tax = 1.1
-    return (price1 + price2) * tax
+    total_price = price1 + price2 
+    print( total_price * tax)
 
 total()
